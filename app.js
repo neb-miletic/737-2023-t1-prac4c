b40fc14b35ffcd50d76f94081e43bc59f8e0579c
neb-miletic <nebojse@gmail.com>
2023-03-25 14:41:59 +1100

    Changes for 4.2C task

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ require('./passport')
 const jwt = require('jsonwebtoken');
 const axios = require('axios')
 const port = 3000;
-
+const auth =  passport.authenticate('jwt', { session: false })
 
 
 // Use Passport.js middleware
 app.use(passport.initialize());
 
 // Define a protected route that requires authentication and authorization
-app.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/protected', auth, (req, res) => {
     res.send('This is a protected route.');
 });
 
@@ -32,57 +32,110 @@ axios.get('http://localhost:3000/protected', {
 
 
 // Addition endpoint with authentication token requested
-app.get('/addition/:num1/:num2',passport.authenticate('jwt', { session: false }), (req, res) => {
-    const num1 = Number(req.params.num1);
-    const num2 = Number(req.params.num2);
+app.get('/addition/:num1/:num2',auth, (req, res) => {
+    try {
+        const num1 = Number(req.params.num1);
+        const num2 = Number(req.params.num2);
 
-    if (isNaN(num1) || isNaN(num2)) {
-        res.status(400).send('Invalid input parameters. Please provide valid numbers.');
-    } else {
-        const result = num1 + num2;
-        res.send(`The result of ${num1} + ${num2} is ${result}.`);
+        if (isNaN((num1))) {
+            throw new Error(res.status(400).send('num1 incorrectly defined.'));
+
+        }
+
+        if (isNaN((num2))) {
+            throw new Error(res.status(400).send('num2 incorrectly defined.'));
+        }
+        if (isNaN(num1) || isNaN(num2)) {
+           throw new Error(res.status(400).send('Invalid input parameters. Please provide valid numbers.'));
+        } else {
+            const result = num1 + num2;
+            res.send(`The result of ${num1} + ${num2} is ${result}.`);
+        }
+    } catch(error) {
+        console.log(error)
+        res.status(500).send("Error while doing the operation")
     }
 });
 
 // Subtraction endpoint  with authentication token requested
-app.get('/subtraction/:num1/:num2',passport.authenticate('jwt', { session: false }), (req, res) => {
-    const num1 = Number(req.params.num1);
-    const num2 = Number(req.params.num2);
+app.get('/subtraction/:num1/:num2', auth, (req, res) => {
+    try {
+        const num1 = Number(req.params.num1);
+        const num2 = Number(req.params.num2);
 
-    if (isNaN(num1) || isNaN(num2)) {
-        res.status(400).send('Invalid input parameters. Please provide valid numbers.');
-    } else {
-        const result = num1 - num2;
-        res.send(`The result of ${num1} - ${num2} is ${result}.`);
+        if (isNaN((num1))) {
+            throw new Error(res.status(400).send('num1 incorrectly defined.'));
+
+        }
+
+        if (isNaN((num2))) {
+            throw new Error(res.status(400).send('num2 incorrectly defined.'));
+        }
+        if (isNaN(num1) || isNaN(num2)) {
+            throw new Error(res.status(400).send('Invalid input parameters. Please provide valid numbers.'));
+        } else {
+            const result = num1 - num2;
+            res.send(`The result of ${num1} - ${num2} is ${result}.`);
+        }
+    } catch(error) {
+        console.log(error)
+        res.status(500).send("Error while doing the operation")
     }
 });
 
 // Multiplication endpoint with authentication token requested
 app.get('/multiplication/:num1/:num2',passport.authenticate('jwt', { session: false }), (req, res) => {
-    const num1 = Number(req.params.num1);
-    const num2 = Number(req.params.num2);
+    try {
+        const num1 = Number(req.params.num1);
+        const num2 = Number(req.params.num2);
 
-    if (isNaN(num1) || isNaN(num2)) {
-        res.status(400).send('Invalid input parameters. Please provide valid numbers.');
-    } else {
-        const result = num1 * num2;
-        res.send(`The result of ${num1} * ${num2} is ${result}.`);
+        if (isNaN((num1))) {
+            throw new Error(res.status(400).send('num1 incorrectly defined.'));
+
+        }
+
+        else if (isNaN((num2))) {
+            throw new Error(res.status(400).send('num2 incorrectly defined.'));
+        }
+        else if (isNaN(num1) || isNaN(num2)) {
+            throw new Error(res.status(400).send('Invalid input parameters. Please provide valid numbers.'));
+        } else {
+            const result = num1 + num2;
+            res.send(`The result of ${num1} * ${num2} is ${result}.`);
+        }
+    } catch(error) {
+        console.log(error)
+        res.status(500).send("Error while doing the operation")
     }
 });
 
 // Division endpoint with authentication token requested
 app.get('/division/:num1/:num2',passport.authenticate('jwt', { session: false }), (req, res) => {
-    const num1 = Number(req.params.num1);
-    const num2 = Number(req.params.num2);
 
-    if (isNaN(num1) || isNaN(num2)) {
-        res.status(400).send('Invalid input parameters. Please provide valid numbers.');
-    } else if (num2 === 0) {
-        res.status(400).send('Division by zero is not allowed.');
-    } else {
-        const result = num1 / num2;
-        res.send(`The result of ${num1} / ${num2} is ${result}.`);
+    try {
+        const num1 = Number(req.params.num1);
+        const num2 = Number(req.params.num2);
+
+        if (isNaN((num1))) {
+            throw new Error(res.status(400).send('num1 incorrectly defined.'));
+        }
+        else if (isNaN((num2))) {
+            throw new Error(res.status(400).send('num2 incorrectly defined.'));
+        }
+        else if (num2 === 0) {
+            res.status(400).send('Division by zero is not allowed.');
+        }
+        else if (isNaN(num1) || isNaN(num2)) {
+            throw new Error(res.status(400).send('Invalid input parameters. Please provide valid numbers.'));
+        } else {
+            const result = num1 / num2;
+            res.send(`The result of ${num1} / ${num2} is ${result}.`);
+        }
+    } catch(error) {
+        console.log(error)
+        res.status(500).send("Error while doing the operation")
     }
+
 });
 
 // Start the server
